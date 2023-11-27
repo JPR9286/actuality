bca5b38131cdc57c1783de8f14b8b270fd89dc38
Jeremy Kwan Chung <jeremykwanchung.1995@gmail.com>
2023-11-27 18:00:33 +0100

    validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
   belongs_to :category
-  has_many :messages
-  
+  validates :title, :atricle_url, presence: true
 end
--- app/models/user_category.rb
+++ app/models/user_category.rb
@@ -1,4 +1,5 @@
 class UserCategory < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  validates :name, presence: true
 end
