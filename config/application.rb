562b3c7c25f59a0e2b4443fe083bdb3a7fc58a56
Adel Jamli <adel.jamli@live.fr>
2023-11-28 18:06:46 +0100

    change

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Actuality
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
+    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
