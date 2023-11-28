aa5ac6f89652964e65d24dd2392ebb39b631f1c5
Adel Jamli <adel.jamli@live.fr>
2023-11-28 12:03:11 +0100

    services to query news api and save in DB

---
+++ app/services/get_news_from_keyword.rb
@@ -0,0 +1,29 @@
+require 'news-api'
+
+class GetNewsFromKeyword
+  def initialize(keyword:, since: Date.today)
+    @keyword = keyword
+    @news_api = News.new(ENV['NEWS_API_KEY']) 
+    @since = since
+  end
+
+  def call
+    @news_api.get_everything(
+      q: @keyword,
+      from: @since.to_s,
+      sortBy: "popularity",
+      language: 'fr'
+    )
+  end
+
+  private
+
+  def from
+    today = Date.today.to_s
+    since = @since.to_s
+    "#{since}&to=#{today}"
+  end
+end
+
+
+# GetNewsFromKeyword.new('kilian').call
