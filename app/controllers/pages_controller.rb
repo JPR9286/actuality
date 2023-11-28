51b391d589f1a2eb4c48fbf7d509b282620f79e0
Adel Jamli <adel.jamli@live.fr>
2023-11-28 17:56:38 +0100

    heroku change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
+require "news-api"
+
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+skip_before_action :authenticate_user!, only: [ :home ]
+
+  def index
+    response = Article.get("https://newsapi.org/v2/top-headlines?country=fr&apiKey=9ceb268dcd214694aaa30cf4ddd4d371")
+    @articles = response.parsed_response['articles']
+  end
 
   def home
   end
+
 end
