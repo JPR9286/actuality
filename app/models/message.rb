73d1a8a66bdd3f34555d3bda50587b13d9e2c811
alexlupu <alexcristi26@yahoo.com>
2023-11-30 12:18:58 +0100

    chat-style

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,9 @@ class Message < ApplicationRecord
   belongs_to :article, optional: true
   belongs_to :chatroom
   validates :content, presence: true
+
+
+  def sender?(a_user)
+    user_id == a_user.id
+  end
 end
