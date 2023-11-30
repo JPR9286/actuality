57bfa4b63abc5506d700d69c4e58482d35a71ef8
Jeremy Kwan Chung <jeremykwanchung.1995@gmail.com>
2023-11-30 12:36:31 +0100

    searchbar with API

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,11 +2,15 @@ class SearchesController < ApplicationController
 
   def index
     if params[:query].present?
+      FetchAndSaveArticlesFromBing.new(keyword: params[:query], freshness: "week").call
       @articles = Article.search_by_title_and_description(params[:query])
       @chatrooms = Chatroom.search_by_title(params[:query])
     else
-      @articles = Article.all
+      @articles = FetchAndSaveArticlesFromBing.new(freshness: "week", category_present: false).call
+      # FetchAndSaveArticlesFromBing.new(freshness: "week")
+      # @articles = Article.order(created_at: :desc)
       @chatrooms = Chatroom.all
     end
   end
+
 end
