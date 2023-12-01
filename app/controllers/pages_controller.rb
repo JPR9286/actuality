1ed15949a5937ee42a85f3b40a0c95cf7489344a
Adel Jamli <adel.jamli@live.fr>
2023-12-01 12:00:04 +0100

    changeconfig

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@
 class PagesController < ApplicationController
 
   def home
-    fetcher = FetchAndSaveArticlesFromBing.new(freshness: "day", category_present: false)
+    fetcher = FetchAndSaveArticlesFromBing.new(freshness: "day", keyword: "sport", category_present: false)
     fetcher.call
     @articles = Article.all
   end
