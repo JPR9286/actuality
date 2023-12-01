bd5e32efeb23324f14967696fc18a8e829579824
Adel Jamli <adel.jamli@live.fr>
2023-12-01 11:17:39 +0100

    temperature

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :user_categories
+  has_many :user_categories, dependent: :destroy
   has_many :articles, dependent: :destroy
   has_many :chatrooms, dependent: :nullify
   validates :name, presence: true, uniqueness: true
