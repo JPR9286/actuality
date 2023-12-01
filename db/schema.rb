0523624f40ac45ce39712476e6c221c33369aed2
Jerome PRINCET <jerome@princet.fr>
2023-12-01 12:04:12 +0100

    update suite main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_154023) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_181720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_154023) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["category_id"], name: "index_chatrooms_on_category_id"
     t.index ["user_id"], name: "index_chatrooms_on_user_id"
   end
