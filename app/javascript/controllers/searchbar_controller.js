1bf1f04f5cd10a829e26ef9f3d459f3b6aca4258
Jeremy Kwan Chung <jeremykwanchung.1995@gmail.com>
2023-12-07 11:03:15 +0100

    chg

--- app/javascript/controllers/searchbar_controller.js
+++ app/javascript/controllers/searchbar_controller.js
@@ -5,22 +5,49 @@ export default class extends Controller {
   static targets = ["searchbar"];
 
   connect() {
-      console.log(window.scrollY)
-      window.addEventListener("scroll", this.changeColor.bind(this));
+    console.log('got it')
+  }
+      // console.log(window.scrollY)
+      // window.addEventListener("scroll", this.changeColor.bind(this));
       // this.handleScroll = this.handleScroll.bind(this);
-
       // window.removeEventListener("scroll", this.handleScroll);
+    // }
 
+    // changeColor(){
+    //   console.log(this.searchbarTarget);
+    //   const searchbar = this.searchbarTarget;
+    //   const scrollPosition = window.scrollY;
+    //   if (scrollPosition > 300) {
+    //     console.log("scroll")
+    //     this.searchbarTarget.style.background = "blue";
+    //   } else {
+    //     this.searchbarTarget.style.background = "#333";
+    //   }
+    // }
+
+    connect() {
+      window.addEventListener("scroll", this.changeColor.bind(this));
+      console.log("Controller connected:", this.element);
     }
 
-    changeColor(){
+    disconnect() {
+      window.removeEventListener("scroll", this.changeColor.bind(this));
+    }
+
+    changeColor() {
       const searchbar = this.searchbarTarget;
+
+      if (!searchbar) {
+        console.warn("Searchbar target not found");
+        return;
+      }
+
       const scrollPosition = window.scrollY;
+
       if (scrollPosition > 300) {
-        console.log("scroll")
-        this.searchbarTarget.style.background = "blue";
+        searchbar.style.background = "blue";
       } else {
-        this.searchbarTarget.style.background = "#333";
+        searchbar.style.background = "#333";
       }
     }
   }
