deb042b5de2d4d9c986f60405c2eb1da98e8d656
Jerome PRINCET <jerome@princet.fr>
2023-12-07 13:12:13 +0100

    changer profil

--- app/controllers/user_categories_controller.rb
+++ app/controllers/user_categories_controller.rb
@@ -1,7 +1,11 @@
 class UserCategoriesController < ApplicationController
   def index
     @user_categories = current_user.categories
-    @user_chatrooms = Chatroom.joins(:messages).where(messages: { user_id: current_user.id }).distinct
+    @user_chatrooms = Chatroom.joins(:messages)
+                   .where(messages: { user_id: current_user.id })
+                   .where.not(user_id: current_user.id)
+                   .distinct
+    @my_chatrooms = Chatroom.where(user_id: current_user.id)
     @shared_articles = Article.joins(:messages).where(messages: { user_id: current_user.id }).distinct
   end
 
