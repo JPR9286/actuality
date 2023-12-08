f03a67e76d539dd0de5a20020d8124bc4d9cba4c
Jerome PRINCET <jerome@princet.fr>
2023-12-08 13:04:40 +0100

    many things

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
   def home
     fetcher = FetchAndSaveArticlesFromBing.new(freshness: "week", category_present: false)
     fetcher.call
-    @articles = Article.where.not(content: nil).order(created_at: :desc)
+    @articles = Article.where.not(content: nil).sort_by { |a| a.date_article }.reverse
   end
 end
